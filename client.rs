af2e670da74a726cc61bb592fa641f3c3e7841db
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-07-10 13:46:00 +0200

    error catching update

--- client.rs
+++ client.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::net::TcpStream;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
+use std::net::{TcpStream, ToSocketAddrs};
 use serde::{Serialize, Deserialize};
 use serde_json;
 
@@ -12,7 +12,7 @@ struct ClientConfig {
 
 impl ClientCommand {
     fn new(cmd: String) -> ClientCommand {
-        ClientCommand{ cmd }
+        ClientCommand { cmd }
     }
 }
 
@@ -27,43 +27,15 @@ struct ServerResponse {
 }
 
 impl ClientConfig {
-    fn new() -> ClientConfig {
-        let server_address = env::var("SERVER_ADDRESS").expect("SERVER_ADDRESS not set in .env");
-        let server_port: u16 = env::var("SERVER_PORT")
-            .expect("SERVER_PORT not set in .env")
-            .parse()
-            .expect("SERVER_PORT must be a valid integer");
+    fn new() -> Result<ClientConfig, env::Var loud client configuration");
 
-        ClientConfig { server_address, server_port }
-    }
-
-    fn connect(&self) -> TcpStream {
-        let connection_string = format!("{}:{}", self.server_address, self.server_port);
-        TcpStream::connect(connection_string)
-            .expect("Failed to connect to the server")
-    }
-
-    fn send_command(&self, command: ClientCommand, mut stream: &TcpStream) {
-        let serialized = serde_json::to_string(&command).unwrap();
-        stream.write(serialized.as_bytes())
-            .expect("Failed to write to server stream");
-    }
-
-    fn receive_response(&self, mut stream: &TcpStream) -> ServerResponse {
-        let mut buffer = Vec::new();
-        stream.read_to_end(&mut buffer).expect("Failed to read from server stream");
-        let response: ServerResponse = serde_json::from_slice(&buffer).expect("Failed to parse server response");
-        response
-    }
-}
-
-fn main() {
-    let client_config = ClientConfig::new();
-    let stream = client_config.connect();
+    let stream = client_config.connect()?;
 
     let command = ClientCommand::new("LIST_FILES".to_string());
-    client_config.send_command(command, &stream);
+    client_config.send_command(command, &stream)?;
 
-    let response = client_process.receive_response(&stream);
+    let response = client_config.receive_response(&stream).expect("Failed to receive response from server");
     println!("Server Response: {:?}", response.message);
+
+    Ok(())
 }
