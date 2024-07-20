fb6a8ce1111a6eb447e2ba7f2d17e090ed34ef25
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-07-20 21:32:44 +0200

    optimize the naming

--- storage.rs
+++ storage.rs
@@ -2,12 +2,13 @@ use std::collections::HashMap;
 use std::env;
 use std::fs::{self, File};
 use std::io::{self, Read, Write};
-use std::path::Path;
+use std::path::PathBuf; 
 
 const BASE_DIR_ENV_KEY: &str = "DFS_BASE_DIR";
 
 struct DistributedFileSystem {
     storage_paths: HashMap<String, String>,
+    base_dir: String, 
 }
 
 impl DistributedFileSystem {
@@ -15,7 +16,7 @@ impl DistributedFileSystem {
         let base_dir = env::var(BASE_DIR_ENV_KEY).unwrap_or_else(|_| "./data".to_string());
         Self {
             storage_paths: HashMap::new(),
-            base_dir,
+            base_dir, 
         }
     }
 
@@ -38,7 +39,7 @@ impl DistributedFileSystem {
         }
     }
 
-    fn delete_file(&mut self, file_name: &str) -> io::result<()> {
+    fn delete_file(&mut self, file_name: &str) -> io::Result<()> {
         if let Some(path) = self.storage_paths.remove(file_name) {
             fs::remove_file(path)?;
             Ok(())
@@ -58,7 +59,9 @@ fn main() {
     let file_name = "example.txt";
     let content = b"Hello, world!";
     dfs.store_file(file_name, content).unwrap();
-    let retrieved_content = dfs.retrieve_timer_file(file_name).unwrap();
+
+    let retrieved_content = dfs.retrieve_file(file_name).unwrap(); 
     println!("Retrieved content: {:?}", String::from_utf8_lossy(&retrieved_content));
+
     dfs.delete_file(file_name).unwrap();
 }
